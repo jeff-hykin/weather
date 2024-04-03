8e6580df646c3e83dc1e6073bb3717b9c7e8ce70
Jeff Hykin <jeff.hykin@gmail.com>
2024-04-02 19:58:09 -0500

    finish

---
+++ components/time_of_day.js
@@ -0,0 +1,277 @@
+import { Chart } from "../generic_tooling/charts.js"
+// import DateTime from "https://deno.land/x/good@1.6.1.3/date.js"
+import { DateTime } from "../imports.js"
+
+
+export const backgroundColorGenerator = (color, {startTime=6, endTime=20, sunriseDuration=1.5}) => {
+    const hour = new Date().getHours()
+    let start, middle, end, angle, percent
+    let angle = (90 * (hour-1 / 23))
+    if (hour < startTime) {
+        start = "rgba(53,51,87,1)"
+        middle = "rgba(38,52,106,1)"
+        end = "rgba(24,52,126,1)"
+        percent = 50
+    } else if (hour < startTime) {
+        start = "rgba(38,52,106,1)"
+        middle = "rgb(235, 127, 32)"
+        end = "rgb(249, 237, 64)"
+        percent = (1-((hour-startTime)/sunriseDuration))*100
+    } else if (hour < 11) {
+        start = "rgba(38,52,106,1)"
+        middle = "rgba(24,52,126,1)"
+        end = "rgba(0,213,255,1)"
+        percent = (1-((hour-startTime)/(endTime-12)))*100
+    }
+    var start = "rgba(38,52,106,1)"
+    var middle = "rgb(235, 127, 32)"
+    var end = "rgb(249, 237, 64)"
+    document.body.style.background = `linear-gradient(${angle}deg, ${start} 0%, ${middle} 47%, ${end} 100%)`
+    return `linear-gradient(${angle}deg, ${start} 0%, ${middle} 47%, ${end} 100%)`
+}
+
+export function HourlyTimeline({periods, style, width, height, showDayNames=true}) {
+    var hourlyData = periods
+    // hourlyData = [
+    //     {
+    //         number: 94,
+    //         name: "",
+    //         startTime: "2024-04-06T11:00:00-05:00",
+    //         endTime: "2024-04-06T12:00:00-05:00",
+    //         isDaytime: true,
+    //         temperature: 72,
+    //         temperatureUnit: "F",
+    //         temperatureTrend: null,
+    //         probabilityOfPrecipitation: { unitCode: "wmoUnit:percent", value: 0 },
+    //         dewpoint: { unitCode: "wmoUnit:degC", value: 15 },
+    //         relativeHumidity: { unitCode: "wmoUnit:percent", value: 63 },
+    //         windSpeed: "20 mph",
+    //         windDirection: "S",
+    //         icon: "https://api.weather.gov/icons/land/day/bkn,0?size=small",
+    //         shortForecast: "Partly Sunny",
+    //         detailedForecast: ""
+    //     },
+    //     {
+    //         number: 95,
+    //         name: "",
+    //         startTime: "2024-04-06T12:00:00-05:00",
+    //         endTime: "2024-04-06T13:00:00-05:00",
+    //         isDaytime: true,
+    //         temperature: 75,
+    //         temperatureUnit: "F",
+    //         temperatureTrend: null,
+    //         probabilityOfPrecipitation: { unitCode: "wmoUnit:percent", value: 0 },
+    //         dewpoint: { unitCode: "wmoUnit:degC", value: 15 },
+    //         relativeHumidity: { unitCode: "wmoUnit:percent", value: 58 },
+    //         windSpeed: "20 mph",
+    //         windDirection: "S",
+    //         icon: "https://api.weather.gov/icons/land/day/bkn,0?size=small",
+    //         shortForecast: "Partly Sunny",
+    //         detailedForecast: ""
+    //     },
+    // ]
+    var dataMax = Math.max(...hourlyData.map(each => each.temperature))
+    var dataMin = Math.min(...hourlyData.map(each => each.temperature))
+    var max = Math.max(dataMax, 100)
+    var min = Math.min(dataMin, 0)
+    var startTimes = hourlyData.map(each => (new DateTime(each.startTime)))
+    var startHours = startTimes.map(each => `${each.hour12}${each.amPm}`)
+    var hasPassedPm = false
+    var passedFirstDay = false
+    var labels = []
+    // for (const each of startTimes) {
+    //     let label = `${each.hour12}${each.amPm}`
+    //     if (each.amPm=="pm") {
+    //         hasPassedPm = true
+    //     }
+    //     if (hasPassedPm && each.amPm=="am") {
+    //         passedFirstDay = true
+    //     }
+    //     if (passedFirstDay) {
+    //         label = `${each.weekDayShort} ${each.hour12}${each.amPm}`
+    //     }
+    //     labels.push(label)
+    // }
+    if (showDayNames) {
+        labels = startTimes.map(each => `${each.weekDayShort} ${each.hour12}${each.amPm}`)
+    } else {
+        labels = startTimes.map(each => `${each.hour12}${each.amPm}`)
+    }
+    let accumulator = 0
+    var chartData = {
+        type: 'line',
+        data: {
+            labels: labels, // ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            datasets: [
+                {
+                    label: 'Temperature (°F)',
+                    yAxisID: 'temperature',
+                    data: hourlyData.map(each => each.temperature),
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    radius: 2,
+                    fill: false
+                },
+                {
+                    label: 'Risk of Rain (%)',
+                    yAxisID: 'rain',
+                    data: hourlyData.map(each => each.probabilityOfPrecipitation.value),
+                    radius: 0,
+                    borderColor: 'rgb(54, 162, 235)',
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                    fill: true
+                },
+                {
+                    label: 'Wind (mph)',
+                    yAxisID: 'wind',
+                    data: hourlyData.map(each => each.windSpeed.replace(/ mph/g, "")-0),
+                    radius: 0,
+                    borderColor: 'rgb(89, 227, 197)',
+                    backgroundColor: 'rgb(89, 227, 197, 0.3)',
+                    fill: true,
+                },
+                {
+                    label: 'Night',
+                    yAxisID: 'dayOrNight',
+                    data: hourlyData.map(each => {
+                        let output = -75*((accumulator>=1)-0) + -25*((accumulator>=2)-0)
+                        if (!each.isDaytime) {
+                            if (accumulator < 2) {
+                                accumulator += 1
+                            }
+                        } else {
+                            if (accumulator > 0) {
+                                accumulator -= 1
+                            }
+                        }
+                        return output
+                    }),
+                    radius: 0,
+                    borderColor: 'rgb(51, 47, 59, 0)',
+                    backgroundColor: 'rgb(51, 47, 59, 0.3)',
+                    tension: 0.4,
+                    fill: true
+                },
+            ]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    // display: false,
+                    fontColor: 'white',
+                    color: 'white',
+                    labels: {
+                        color: 'white',
+                    },
+                    font: {
+                        size: 14,
+                        color: 'white',
+                    }
+                },
+            },
+            layout: {
+                padding: 20
+            },
+            scales: {
+                x: {
+                    grid: {
+                        // display: false,
+                        // drawOnChartArea: false,
+                        // drawTicks: false,
+                        color: 'rgb(51, 47, 59, 0)',
+                    },
+                    ticks: {
+                        color: 'rgb(255, 255, 255)',
+                        callback: function(val, index) {
+                            // Hide every 2nd tick label
+                            return index % 2 === 0 ? this.getLabelForValue(val) : '';
+                        },
+                        font: {
+                            size: 14
+                        },
+                    }
+                },
+                wind: {
+                    max: 100,
+                    min: 0,
+                    type: 'linear',
+                    position: 'left',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Wind (mph)',
+                        color: 'rgb(255, 255, 255)',
+                    },
+                    ticks: {
+                        color: 'rgb(255, 255, 255)',
+                        display: false
+                    },
+                },
+                temperature: {
+                    max,
+                    min,
+                    id: 'temperature',
+                    type: 'linear',
+                    position: 'left',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Temperature (°F)'
+                    },
+                    ticks: {
+                        display: true,
+                        callback: function(value, index, values) {
+                            return index % 2 === 0 ? "         "+value + '°F' : '';
+                        },
+                        color: 'rgb(255, 255, 255)',
+                        font: {
+                            size: 14,
+                        },
+                    },
+                    grid: {
+                         color: 'rgb(255, 255, 255, 0.3)',
+                    },
+                },
+                rain: {
+                    max: 100,
+                    min: 0,
+                    type: 'linear',
+                    position: 'left',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Risk of Rain (%)'
+                    },
+                    ticks: {
+                        display: false
+                    },
+                    grid: {
+                         color: 'rgb(51, 47, 59, 0)',
+                    },
+                },
+                dayOrNight: {
+                    max: 0,
+                    min: -100,
+                    type: 'linear',
+                    position: 'left',
+                    scaleLabel: {
+                        display: false,
+                    },
+                    ticks: {
+                        color: 'rgb(255, 255, 255)',
+                        display: false
+                    },
+                    grid: {
+                         color: 'rgb(51, 47, 59, 0)',
+                    },
+                }
+            }
+        },
+    }
+    const canvas =  Chart({
+        style,
+        width,
+        height,
+        data: chartData,
+    })
+    canvas.style.marginLeft = "-58px"
+    
+    return canvas
+}
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { fadeIn, fadeOut } from "https://deno.land/x/good_component@0.2.12/main/
 import { showToast } from "https://deno.land/x/good_component@0.2.12/main/actions.js"
 import { addDynamicStyleFlags, setupStyles, createCssClass, setupClassStyles, hoverStyleHelper, combineClasses, mergeStyles, AfterSilent, removeAllChildElements } from "https://deno.land/x/good_component@0.2.12/main/helpers.js"
 import { zip, enumerate, count, permute, combinations, wrapAroundGet } from "https://deno.land/x/good@1.5.1.0/array.js"
-
+window.wrapAroundGet = wrapAroundGet
 import storageObject from "https://deno.land/x/storage_object@0.0.2.0/main.js"
 import { HourlyTimeline } from "./components/hourly.js"
 
@@ -24,23 +24,50 @@ var todayLimit = (new DateTime()).add({days:1}).unix
 var todaysHourlyData = hourlyData.filter(each => (new DateTime(each.startTime)).unix<todayLimit)
 var otherHourlyData = hourlyData.filter(each => (new DateTime(each.startTime)).unix>=todayLimit)
 let column
+// pink color
+
+const colors = ["rgba(24,52,126,1)","rgba(24,52,126,1)","rgba(24,52,126,1)","rgba(24,52,126,1)","#5b73df", "#897ed3","#cf5c5c","#c19b4a","#498ada","#208ea2","#64b3d9","#64b3d9","#64b3d9","#64b3d9","#64b3d9","#64b3d9","#c19b4a","#cf5c5c","#897ed3","rgba(24,52,126,1)","rgba(24,52,126,1)","rgba(24,52,126,1)","rgba(24,52,126,1)","rgba(24,52,126,1)",]
+let nextColors = []
+let index = -1
+for (const each of colors) {
+    nextColors.push(
+        wrapAroundGet((new Date().getHours())+index, colors)
+    )
+    index+=1
+}
 document.body = html`
     <body
         font-size=15px
         background-color=whitesmoke
         overflow=scroll
         width=100vw
-        background="rgb(53,51,87)";
-        background="linear-gradient(21deg, rgba(53,51,87,1) 0%, rgba(24,52,126,1) 11%, rgba(0,213,255,1) 100%)"
         display="flex"
         alignItems="center"
         flexDirection="column"
+        transition="background 360s ease-in-out"
+        class="stage"
         >
-            ${column = html`<Column width="fill-available" flex-grow=1 align-items="center" horizontalAlignment="center">
-                <Row flex-grow=1 style="width: 55rem; max-width: calc(90vw - 1rem); margin: 2rem; background-color: rgba(255,255,255,0.2); box-sizing: content-box; padding: 1rem; border-radius: 0.5rem; box-shadow: 0 4px 5px 0 rgba(0,0,0,0.10),0 1px 10px 0 rgba(0,0,0,0.08),0 2px 4px -1px rgba(0,0,0,0.24);" verticalAlignment="center" horizontalAlignment="center" overflow="hidden" position="relative">
+        <style>
+            body {
+                margin: 0;
+            }
+            .stage {
+                animation: animateBg 86400s linear infinite;
+                background-image: linear-gradient(201deg,${nextColors.join(",")});
+                background-size: 100% 1500%;
+                height: 100vh;
+                width: 100vw;
+            }
+            @keyframes animateBg {
+                0% { background-position: 0% 0%; }
+                100% { background-position: 0% 100%; }
+            }
+        </style>
+            ${column = html`<Column width="fill-available" flex-grow=1 align-items="center" horizontalAlignment="center" height=200vh>
+                <Row flex-grow=1 style="width: 55rem; max-width: calc(90vw - 1rem); margin: 2rem; background-color: rgba(255,255,255,0.2); box-sizing: content-box; padding: 1rem; border-radius: 0.5rem; box-shadow: 0 4px 5px 0 rgba(0,0,0,0.10),0 1px 10px 0 rgba(0,0,0,0.08),0 2px 4px -1px rgba(0,0,0,0.24);" verticalAlignment="center" horizontalAlignment="center" overflow="hidden" position="relative" max-height=30rem>
                     <HourlyTimeline periods=${todaysHourlyData} width=500px height=300px  showDayNames=${false}/>
                 </Row>
-                <Row flex-grow=1 style="width: 55rem; max-width: calc(90vw - 1rem); margin: 2rem; background-color: rgba(255,255,255,0.2); box-sizing: content-box; padding: 1rem; border-radius: 0.5rem; box-shadow: 0 4px 5px 0 rgba(0,0,0,0.10),0 1px 10px 0 rgba(0,0,0,0.08),0 2px 4px -1px rgba(0,0,0,0.24);" verticalAlignment="center" horizontalAlignment="center" overflow="hidden" position="relative">
+                <Row flex-grow=1 style="width: 55rem; max-width: calc(90vw - 1rem); margin: 2rem; background-color: rgba(255,255,255,0.2); box-sizing: content-box; padding: 1rem; border-radius: 0.5rem; box-shadow: 0 4px 5px 0 rgba(0,0,0,0.10),0 1px 10px 0 rgba(0,0,0,0.08),0 2px 4px -1px rgba(0,0,0,0.24);" verticalAlignment="center" horizontalAlignment="center" overflow="hidden" position="relative" max-height=30rem>
                     <HourlyTimeline periods=${otherHourlyData} width=500px height=300px  />
                 </Row>
             </Column>`}
